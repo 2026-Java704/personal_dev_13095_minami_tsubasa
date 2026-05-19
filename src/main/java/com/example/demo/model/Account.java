3ccc8f856d9b9e84014dfd21a32774e219ea2f38
student <student@FJ-1025>
2026-05-19 15:51:59 +0900

    一覧表の新規追加でユーザIDを保持するためにセッションのAccountJavaを追加。

---
+++ src/main/java/com/example/demo/model/Account.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class Account {
+
+	private String name;
+	private Integer id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+}
